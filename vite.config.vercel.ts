3edfec1ddd411072c5853682cca4630d4cf379ed
Reza Jafar <admin@rezajafar.com>
2024-10-29 01:09:12 -0700

    many changes

---
+++ vite.config.vercel.ts
@@ -0,0 +1,27 @@
+import { vitePlugin as remixVitePlugin } from '@remix-run/dev';
+import UnoCSS from 'unocss/vite';
+import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { optimizeCssModules } from 'vite-plugin-optimize-css-modules';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  build: {
+    target: 'esnext',
+  },
+  plugins: [
+    nodePolyfills({
+      include: ['path', 'buffer'],
+    }),
+    remixVitePlugin({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+    }),
+    UnoCSS(),
+    tsconfigPaths(),
+    optimizeCssModules({ apply: 'build' }),
+  ],
+});
