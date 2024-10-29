ff9f2f4237a4f46d63fdf53c3a781047e0d05b94
Reza Jafar <admin@rezajafar.com>
2024-10-29 01:57:00 -0700

    entry

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,7 +1,7 @@
 import type { AppLoadContext, EntryContext } from '@remix-run/cloudflare';
 import { RemixServer } from '@remix-run/react';
-import { isbot } from 'isbot';
-import * as ReactDOMServer from 'react-dom/server';
+import isbot from 'isbot';
+import { renderToReadableStream } from 'react-dom/server';
 import { renderHeadToString } from 'remix-island';
 import { Head } from './root';
 import { themeStore } from '~/lib/stores/theme';
@@ -16,16 +16,13 @@ export default async function handleRequest(
 ) {
   await initializeModelList();
 
-  const readable = await ReactDOMServer.renderToReadableStream(
-    <RemixServer context={remixContext} url={request.url} />,
-    {
-      signal: request.signal,
-      onError(error: unknown) {
-        console.error(error);
-        responseStatusCode = 500;
-      },
+  const readable = await renderToReadableStream(<RemixServer context={remixContext} url={request.url} />, {
+    signal: request.signal,
+    onError(error: unknown) {
+      console.error(error);
+      responseStatusCode = 500;
     },
-  );
+  });
 
   const body = new ReadableStream({
     start(controller) {
@@ -68,7 +65,7 @@ export default async function handleRequest(
     },
   });
 
-  if (isbot(request.headers.get('user-agent') || '')) {
+  if (isbot.isbot(request.headers.get('user-agent') || '')) {
     await readable.allReady;
   }
 
